858ab3c60d107bd4be626ce2b3eff05a30bf50c7
Yousef <matinfard.y@gmail.com>
2024-10-12 17:08:01 +0330

    Add configure Kotlin Android settings and enable Java 17

--- build-logic/convention/src/main/java/com/matin/amazingshop/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/matin/amazingshop/KotlinAndroid.kt
@@ -1,9 +1,11 @@
 package com.matin.amazingshop
 
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -41,4 +43,32 @@ internal fun Project.configureKotlinJvm() {
     }
 
     configureKotlin()
+}
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 34
+
+        defaultConfig {
+            minSdk = 26
+        }
+
+        compileOptions {
+            // Up to Java 11 APIs are available through desugaring
+            // https://developer.android.com/studio/write/java11-minimal-support-table
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+            isCoreLibraryDesugaringEnabled = true
+        }
+
+
+    }
+
+    configureKotlin()
+
+    dependencies {
+        add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+    }
 }

3592c24fe7bd67724383e1e5b890254cbe5842d1
Yousef <matinfard.y@gmail.com>
2024-10-12 13:54:37 +0330

    initial commit

---
+++ app/src/main/java/com/matin/amazingshop/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.matin.amazingshop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.matin.amazingshop.ui.theme.AmazingShopTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AmazingShopTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}

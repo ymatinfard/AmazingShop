832a51a31883aa4069bebf5afcf3aea24d5cae49
Yousef <matinfard.y@gmail.com>
2024-10-12 19:46:38 +0330

    Add network module
    - Add retrofit instance
    - Add getProducts api

---
+++ core/network/src/main/java/com/matin/amazingshop/core/network/AmazingShopApi.kt
@@ -0,0 +1,9 @@
+package com.matin.amazingshop.core.network
+
+import com.matin.amazingshop.core.network.model.NetworkProductsData
+import retrofit2.http.GET
+
+interface AmazingShopApi {
+    @GET("v3/3508850b-c63d-47b2-b1f8-b559cdd5327d")
+    fun getProducts(): NetworkProductsData
+}
---
+++ core/network/src/main/java/com/matin/amazingshop/core/network/di/NetworkModule.kt
@@ -0,0 +1,23 @@
+package com.matin.amazingshop.core.network.di
+
+import com.matin.amazingshop.core.network.AmazingShopApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): AmazingShopApi {
+        return Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(AmazingShopApi::class.java)
+    }
+}

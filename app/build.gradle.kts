5db475c961d64853bbf729dcdb9f32a1530be8a7
Yousef <matinfard.y@gmail.com>
2024-10-13 13:17:33 +0330

    Add compose dependency in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

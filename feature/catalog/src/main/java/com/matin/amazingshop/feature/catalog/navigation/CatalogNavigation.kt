c09920ed84965a4310f2f3f74ab573313b933043
Yousef <matinfard.y@gmail.com>
2024-10-13 20:48:39 +0330

    Add CatalogScreen navigation

---
+++ feature/catalog/src/main/java/com/matin/amazingshop/feature/catalog/navigation/CatalogNavigation.kt
@@ -0,0 +1,20 @@
+package com.matin.amazingshop.feature.catalog.navigation
+
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.matin.amazingshop.feature.catalog.CatalogScreen
+import com.matin.amazingshop.feature.catalog.CatalogScreenViewModel
+
+const val CATALOG_ROUTE = "catalog_route"
+
+fun NavController.navigateToCatalog(navOptions: NavOptions) = navigate(CATALOG_ROUTE, navOptions)
+
+fun NavGraphBuilder.catalogScreen() {
+    return composable(CATALOG_ROUTE) {
+        val viewModel = hiltViewModel<CatalogScreenViewModel>()
+        CatalogScreen(viewModel)
+    }
+}

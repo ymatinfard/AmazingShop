915ad8becca0618522d2103741142b6589465ac6
Yousef <matinfard.y@gmail.com>
2024-10-14 15:38:29 +0330

    Add database module as dependency to data module

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -11,5 +11,7 @@ dependencies {
     implementation(projects.core.model)
     implementation(projects.core.network)
     implementation(projects.core.common)
+    implementation(projects.core.model)
+    implementation(projects.core.database)
     implementation(libs.kotlinx.coroutines.test)
 }

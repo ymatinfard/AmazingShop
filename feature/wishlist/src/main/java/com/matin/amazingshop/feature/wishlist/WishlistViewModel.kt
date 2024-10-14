b7d336c6e31a5d484f284c8ea6dfb09c814c08f4
Yousef <matinfard.y@gmail.com>
2024-10-14 14:07:33 +0330

    Add initial wishlist viewmodel

---
+++ feature/wishlist/src/main/java/com/matin/amazingshop/feature/wishlist/WishlistViewModel.kt
@@ -0,0 +1,9 @@
+package com.matin.amazingshop.feature.wishlist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WishlistViewModel @Inject constructor(): ViewModel() {
+}

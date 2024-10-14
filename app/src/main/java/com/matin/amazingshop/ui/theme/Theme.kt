dc807e8862ce094f0af51df37efbca22a938c4dd
Yousef <matinfard.y@gmail.com>
2024-10-14 12:30:40 +0330

    Add Topbar in CatalogScreen

--- app/src/main/java/com/matin/amazingshop/ui/theme/Theme.kt
+++ app/src/main/java/com/matin/amazingshop/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 fun AmazingShopTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
--- feature/catalog/src/main/java/com/matin/amazingshop/feature/catalog/CatalogScreen.kt
+++ feature/catalog/src/main/java/com/matin/amazingshop/feature/catalog/CatalogScreen.kt
@@ -1,5 +1,6 @@
 package com.matin.amazingshop.feature.catalog
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -26,10 +27,12 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -43,11 +46,44 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.matin.amazingshop.core.model.Item
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 fun CatalogScreen(viewModel: CatalogScreenViewModel) {
     val catalogState = viewModel.catalogUiState.collectAsStateWithLifecycle()
-    Surface(modifier = Modifier.fillMaxSize()) {
-        CatalogScreenContent(catalogState.value) { favItemId -> }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            TopBar()
+            CatalogScreenContent(catalogState.value) { favItemId -> }
+        }
+}
+
+@Composable
+fun TopBar() {
+    ElevatedCard(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(0),
+    ) {
+        Box(
+            modifier = Modifier
+                .height(46.dp)
+                .fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 16.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Icon(imageVector = Icons.Default.Favorite, contentDescription = null)
+            }
+
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Text(text = "NEW IN")
+            }
+        }
     }
 }
 
@@ -58,6 +94,7 @@ fun CatalogScreenContent(catalog: CatalogUiState, onFavoriteItemClick: (String) 
         is CatalogUiState.Success -> {
             Catalog(catalog, onFavoriteItemClick)
         }
+
         is CatalogUiState.Error -> {
             Text(text = "Error: ${catalog.message}")
         }
@@ -69,9 +106,8 @@ private fun Catalog(
     catalog: CatalogUiState.Success,
     onFavoriteItemClick: (String) -> Unit
 ) {
-    BoxWithConstraints {
+    BoxWithConstraints(modifier = Modifier.padding(horizontal = 16.dp, vertical = 6.dp)) {
         LazyVerticalGrid(
-            modifier = Modifier.padding(16.dp),
             columns = GridCells.Fixed(2),
             verticalArrangement = Arrangement.spacedBy(3.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
@@ -94,8 +130,7 @@ fun CatalogItem(
         ElevatedCard(
             shape = RoundedCornerShape(2.dp),
             elevation = CardDefaults.elevatedCardElevation(8.dp),
-            onClick = { /*TODO*/ },
-            colors = CardDefaults.elevatedCardColors(containerColor = Color.White)
+            onClick = { /*TODO*/ }
         ) {
             Column(modifier = Modifier.padding(8.dp)) {
                 Row(

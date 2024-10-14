c75c9dc52cf5fd6819fba37967903b4add4c0ddb
Yousef <matinfard.y@gmail.com>
2024-10-14 15:40:42 +0330

    Add database add,remove and fetch methods in repository

--- core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepository.kt
+++ core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepository.kt
@@ -1,8 +1,12 @@
 package com.matin.amazingshop.core.data
 
-import com.matin.amazingshop.core.model.ProductsData
+import com.matin.amazingshop.core.model.Catalog
+import com.matin.amazingshop.core.model.Item
 import kotlinx.coroutines.flow.Flow
 
 interface AmazingShopRepository {
-    fun getCatalog(): Flow<ProductsData>
+    fun getCatalog(): Flow<Catalog>
+    fun getWishlist(): Flow<List<Item>>
+    suspend fun addToWishlist(item: Item)
+    suspend fun removeFromWishlist(item: Item)
 }
--- core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepositoryImpl.kt
+++ core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepositoryImpl.kt
@@ -1,19 +1,34 @@
 package com.matin.amazingshop.core.data
 
-import com.matin.amazingshop.core.common.network.AmazingShopDispatcher.IO
-import com.matin.amazingshop.core.common.network.Dispatcher
-import com.matin.amazingshop.core.model.ProductsData
+import com.matin.amazingshop.core.database.WishlistDao
+import com.matin.amazingshop.core.database.toDomain
+import com.matin.amazingshop.core.database.toEntity
+import com.matin.amazingshop.core.model.Catalog
+import com.matin.amazingshop.core.model.Item
 import com.matin.amazingshop.core.network.AmazingShopApi
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AmazingShopRepositoryImpl @Inject constructor(
-    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
-    private val api: AmazingShopApi
+    private val api: AmazingShopApi,
+    private val dao: WishlistDao,
 ) : AmazingShopRepository {
-    override fun getCatalog(): Flow<ProductsData> = flow {
+
+    override fun getCatalog(): Flow<Catalog> = flow {
         emit(api.getCatalog().toDomain())
+    }
+
+    override fun getWishlist(): Flow<List<Item>> =
+        dao.getWishlist().map { it.map { it.toDomain() } }
+
+
+    override suspend fun addToWishlist(item: Item) {
+        dao.addToWishlist(item.toEntity())
+    }
+
+    override suspend fun removeFromWishlist(item: Item) {
+        dao.removeFromWishlist(item.toEntity())
     }
 }

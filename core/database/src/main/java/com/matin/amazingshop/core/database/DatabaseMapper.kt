5f2d63c0585b5b294efb60510654008deeca2e6b
Yousef <matinfard.y@gmail.com>
2024-10-14 15:39:25 +0330

    Add database entity mapper

---
+++ core/database/src/main/java/com/matin/amazingshop/core/database/DatabaseMapper.kt
@@ -0,0 +1,26 @@
+package com.matin.amazingshop.core.database
+
+import com.matin.amazingshop.core.model.Image
+import com.matin.amazingshop.core.model.Item
+
+fun Item.toEntity(): WishEntity {
+    return WishEntity(
+        id = id,
+        name = name,
+        brand = brand,
+        image = image.url,
+        price = price,
+    )
+}
+
+fun WishEntity.toDomain(): Item {
+    return Item(
+        id = id,
+        name = name,
+        brand = brand,
+        image = Image(image, 0, 0),
+        price = price,
+        sku = "",
+        badges = arrayListOf(),
+    )
+}

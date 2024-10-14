1c06909c3afafc982ef8e8592f924db4a95ce599
Yousef <matinfard.y@gmail.com>
2024-10-14 14:07:21 +0330

    Add initial wishlist screen

---
+++ feature/wishlist/src/main/java/com/matin/amazingshop/feature/wishlist/WishlistScreen.kt
@@ -0,0 +1,10 @@
+package com.matin.amazingshop.feature.wishlist
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+
+@Composable
+fun WishlistScreen(viewModel: ViewModel) {
+    Text(text = "Whishlist Screen")
+}

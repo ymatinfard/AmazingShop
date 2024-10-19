29edb0b54fbc95bc87c48682512082697082c91b
Yousef <matinfard.y@gmail.com>
2024-10-19 12:56:39 +0200

    Use sharemodel for catalog screen and item detail screen

--- app/src/main/java/com/matin/amazingshop/navigation/AmazingShopNavHost.kt
+++ app/src/main/java/com/matin/amazingshop/navigation/AmazingShopNavHost.kt
@@ -1,19 +1,28 @@
 package com.matin.amazingshop.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.matin.amazingshop.feature.catalog.CatalogViewModel
 import com.matin.amazingshop.feature.catalog.navigation.CATALOG_ROUTE
 import com.matin.amazingshop.feature.catalog.navigation.catalogScreen
+import com.matin.amazingshop.feature.catalog.navigation.itemDetailScreen
+import com.matin.amazingshop.feature.catalog.navigation.navigateToItemDetail
 import com.matin.amazingshop.feature.wishlist.navigation.navigateToWishlist
 import com.matin.amazingshop.feature.wishlist.navigation.wishlistScreen
 
 @Composable
 fun AmazingShopNavHost() {
     val navController: NavHostController = rememberNavController()
+    val catalogViewModel: CatalogViewModel = hiltViewModel()
     NavHost(navController = navController, startDestination = CATALOG_ROUTE) {
-        catalogScreen(onWishlistClick = { navController.navigateToWishlist() })
-        wishlistScreen()
+        catalogScreen(
+            sharedViewModel = catalogViewModel,
+            onWishlistClick = { navController.navigateToWishlist() },
+            onItemClick = { navController.navigateToItemDetail() })
+        wishlistScreen { navController.popBackStack() }
+        itemDetailScreen(catalogViewModel) { navController.popBackStack() }
     }
 }

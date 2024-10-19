3d50f92623d741ab93c729febe0bccd46fd33d09
Yousef <matinfard.y@gmail.com>
2024-10-19 13:41:55 +0200

    Add loading wheel

--- feature/catalog/src/main/java/com/matin/amazingshop/feature/catalog/CatalogScreen.kt
+++ feature/catalog/src/main/java/com/matin/amazingshop/feature/catalog/CatalogScreen.kt
@@ -1,6 +1,8 @@
 package com.matin.amazingshop.feature.catalog
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -28,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -40,6 +43,7 @@ import coil3.compose.AsyncImage
 import com.matin.amazingshop.core.designsystem.component.Banner
 import com.matin.amazingshop.core.designsystem.component.ItemBadge
 import com.matin.amazingshop.core.designsystem.component.ItemSpec
+import com.matin.amazingshop.core.designsystem.component.LoadingWheel
 import com.matin.amazingshop.core.designsystem.component.TopAppBar
 import com.matin.amazingshop.core.designsystem.component.WishlistIcon
 import com.matin.amazingshop.core.model.Item
@@ -83,13 +87,18 @@ fun CatalogScreenContent(
     onWishlistClick: ClickListener
 ) {
     when (catalog) {
-        is CatalogUiState.Loading -> Text(text = "Loading...")
+        is CatalogUiState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                LoadingWheel()
+            }
+        }
+
         is CatalogUiState.Success -> {
             Catalog(catalog, onItemClick, onWishlistClick)
         }
 
         is CatalogUiState.Error -> {
-            Text(text = "Error: ${catalog.message}")
+            Log.e("Error", catalog.message)
         }
     }
 }

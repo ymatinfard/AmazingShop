09962150c679c6ea0f4eba49dc5d6e28b8ea8fc2
Yousef <matinfard.y@gmail.com>
2024-11-22 17:45:31 +0100

    Synchronise item status update

--- core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepositoryImpl.kt
+++ core/data/src/main/java/com/matin/amazingshop/core/data/AmazingShopRepositoryImpl.kt
@@ -9,12 +9,15 @@ import com.matin.amazingshop.core.network.AmazingShopApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class AmazingShopRepositoryImpl @Inject constructor(
     private val api: AmazingShopApi,
     private val dao: ItemStatusDao,
 ) : AmazingShopRepository {
+    val mutex = Mutex()
 
     override fun getCatalog(): Flow<Catalog> = flow {
         emit(api.getCatalog().toDomain())
@@ -27,34 +30,38 @@ class AmazingShopRepositoryImpl @Inject constructor(
         dao.getItemsStatus().map { it.map { it.toDomain() } }
 
     override suspend fun toggleWishlist(item: Item) {
-        val existingItem = dao.getItemById(item.id)
-        if (existingItem != null) {
-            val newWishlistStatus = !existingItem.isInWishlist
-            dao.updateWishlistStatus(item.id, newWishlistStatus)
+        mutex.withLock {
+            val existingItem = dao.getItemById(item.id)
+            if (existingItem != null) {
+                val newWishlistStatus = !existingItem.isInWishlist
+                dao.updateWishlistStatus(item.id, newWishlistStatus)
 
-            // After toggling, check if we need to remove the item
-            if (!existingItem.isInCart && !newWishlistStatus) {
-                // Both flags are false, remove the item from the database
-                dao.removeItemById(item.id)
+                // After toggling, check if we need to remove the item
+                if (!existingItem.isInCart && !newWishlistStatus) {
+                    // Both flags are false, remove the item from the database
+                    dao.removeItemById(item.id)
+                }
+            } else {
+                dao.insert(item.copy(isInWishlist = true).toEntity())
             }
-        } else {
-            dao.insert(item.copy(isInWishlist = true).toEntity())
         }
     }
 
     override suspend fun toggleCart(item: Item) {
-        val existingItem = dao.getItemById(item.id)
-        if (existingItem != null) {
-            val newCartStatus = !existingItem.isInCart
-            dao.updateCartStatus(item.id, newCartStatus)
+        mutex.withLock {
+            val existingItem = dao.getItemById(item.id)
+            if (existingItem != null) {
+                val newCartStatus = !existingItem.isInCart
+                dao.updateCartStatus(item.id, newCartStatus)
 
-            // After toggling, check if we need to remove the item
-            if (!existingItem.isInWishlist && !newCartStatus) {
-                // Both flags are false, remove the item from the database
-                dao.removeItemById(item.id)
+                // After toggling, check if we need to remove the item
+                if (!existingItem.isInWishlist && !newCartStatus) {
+                    // Both flags are false, remove the item from the database
+                    dao.removeItemById(item.id)
+                }
+            } else {
+                dao.insert(item.copy(isInCart = true).toEntity())
             }
-        } else {
-            dao.insert(item.copy(isInCart = true).toEntity())
         }
     }
 }
